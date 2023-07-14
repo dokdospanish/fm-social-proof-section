b7d7e280e8700c5fba7c4f9dbd5adc80dd681e80
Casa de Dokdo <dokdospanish@gmail.com>
2023-07-14 15:21:39 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['League Spartan', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        "custom-dark-magenta": "hsl(300, 43%, 22%)",
+        "custom-pink": "hsl(333, 80%, 67%)",
+        "custom-light-magenta": "hsl(300, 24%, 96%)",
+        "custom-magenta": "hsl(303, 10%, 53%)"
+      }
+    },
+  },
+  plugins: [],
+}
+
